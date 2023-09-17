f1571cd2cdfeccb41b098bc12a49a8be77a61945
vdella <vitor.origamer@gmail.com>
2023-09-17 02:50:50 -0300

    added Header CSS style with mui/styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 
+// import { Autocomplete } from '@react-google-maps/api';
+
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@mui/material';
+import SearchIcon from '@mui/material/Icon';
+
+import useStyles from './styles';
+
 const Header = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>
-                Carte
-            </h1>
-        </div>
+        <AppBar position="static">
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h5" className={classes.title}>
+                    Carte
+                </Typography>
+                <Box display="flex">
+                    <Typography variant="h6" className={classes.title}>
+                    Type in the name of where you to go!
+                    </Typography>
+                    {/*<Autocomplete>*/}
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
+                            </div>
+                            <InputBase placeholder="Search..." classes={{root: classes.inputRoot, input: classes.inputInput}} />
+                        </div>
+                    {/*</Autocomplete>*/}
+                </Box>
+            </Toolbar>
+        </AppBar>
     )
 }
 
