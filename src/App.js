998c96e30a48b5f3aad62fcf663d4028cac30598
vdella <vitor.origamer@gmail.com>
2023-09-17 01:07:46 -0300

    added Header, List and Map components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
 const App = () => {
+    const fullMobileWidth = 12;  // As an arbitrary width for small devices.
+    const listOfPlacesWidth = fullMobileWidth / 3;
+    const wholeMapWidth = 2 * listOfPlacesWidth;  // Gets 2/3 of the screen.
+
     return (
-        <div>
-            <h1>
-                Hello, World!
-            </h1>
-        </div>
+        <>
+        <CssBaseline />
+        <Header />
+            <Grid container spacing={3} style={{ width: '100%' }}>
+                <Grid item xs={fullMobileWidth} md={listOfPlacesWidth}>
+                    <List />
+                </Grid>
+                <Grid item xs={fullMobileWidth} md={wholeMapWidth}>
+                    <Map />
+                </Grid>
+            </Grid>
+        </>
     )
 }
 
