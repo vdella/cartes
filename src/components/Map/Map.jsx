1fafab5aebacb2991bfbe92e1f3c5eca61a4a0e4
vdella <vitor.origamer@gmail.com>
2023-09-19 02:22:24 -0300

    added Map component
    	* In order to work, it needs a bootstrap URL key given by
    	  Google Cloud. The key remains as a string literal yet to
    	  be changed to something secure and hidden to outsiders;
    	  for testing purposes, it is enough.
    	* Parameters options, onChange and onChildClick will be properly
    	  filled according to the guide.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+
+import useStyles from './styles';
 
 const Map = () => {
+    const classes = useStyles();
+
+    const coordinates = { lat: 0, lng: 0}  // As the latitude and longitude.
+
     return (
-        <div>
-            <h1>
-                Whole map.
-            </h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyB91jVhjAoGNLQz70UMIyhvdBujZi3uxbk' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
         </div>
     )
 }
