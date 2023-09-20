c12d7974326bb09042c0cf63d00d80b4d080ccc0
vdella <vitor.origamer@gmail.com>
2023-09-20 16:32:55 -0300

    added simple React list component
    	* The list of places is built around restaurants, hotels
    	and attractions, followed by their related ratings. By now,
    	the actual list does not contain real places with their
    	real ratings. This will soon be added with the use of
    	a proper API.
    	* The list, so far, is intended to be seen as component
    	yet to be further worked on and finished.
    	* Using a Select HTML component, places and ratings work
    	around a hook function, which maps, for instance, a type
    	of place (e.g. Restaurant) to the name of an actual restaurant.

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
+
+import useStyles from './styles';
 
 const List = () => {
+    const classes = useStyles();
+
+    // Types of places and their ratings act as mapping function. They are properly called onChange
+    // of their own Select component.
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState('');
+
+    // TODO add meaningful place names with the use of an place list API.
+    const places = [
+        { name: 'Place' },
+        { name: 'Best coffee' },
+        { name: 'Best tuna' }
+    ];
+
     return (
-        <div>
-            <h1>
-                List of places.
-            </h1>
+        <div className={classes.container}>
+            <Typography variant="h4">Restaurants, hotels & attractions around you</Typography>
+
+            <FormControl className={classes.formControl}>
+                <InputLabel>Type</InputLabel>
+                <Select value={type} onChange={(e) => setType(e.target.value)}>
+                    <MenuItem value="restaurants">Restaurants</MenuItem>
+                    <MenuItem value="hotels">Hotels</MenuItem>
+                    <MenuItem value="attractions">Attractions</MenuItem>
+                </Select>
+            </FormControl>
+
+            <FormControl className={classes.formControl}>
+                <InputLabel>Rating</InputLabel>
+                <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+                    <MenuItem value={0}>All</MenuItem>
+                    <MenuItem value={3}>Above 3 stars</MenuItem>
+                    <MenuItem value={4}>Above 4 stars</MenuItem>
+                    <MenuItem value={4.5}>Above 4.5 stars</MenuItem>
+                </Select>
+            </FormControl>
+
+            <Grid container spacing={3} className={classes.list}>
+                {places?.map((place, index) => (
+                    <Grid item key={index} xs={12}>
+                        <PlaceDetails place={place} />
+                    </Grid>
+                ))}
+            </Grid>
         </div>
     )
 }
